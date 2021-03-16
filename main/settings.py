591dc04c9e0843004d011415221311fcdc1376df
NasipCOM <salmahunov@mail.ru>
2021-03-16 23:03:27 +0600

    heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jx-+zf@gixi@s=r$^y&#7o$m2+pt%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vs-back.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
