ec37636abb54e289a540bdfe1d335a877e43c62f
NasipCOM <salmahunov@mail.ru>
2021-04-05 16:53:02 +0600

    Debug = True for static

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jx-+zf@gixi@s=r$^y&#7o$m2+pt%7*sc&pbjdloxty7=lqsr3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['vs-back.herokuapp.com','127.0.0.1']
